04e287c3efbcff86fda0f0f049a7e2031cace0de
col-a-guo <collinguo.d20@gmail.com>
2024-11-14 23:49:02 -0800

    fixed??

--- cyclopssim.py
+++ cyclopssim.py
@@ -27,10 +27,10 @@ for build_id in build_ids:
     #cd boots-book-lightning truncheon-genius wand-concentrated energy-holy crystal-magic pot
     #vs
     #cd boots-book-book of sages-genius wand-concentrated energy-holy crystal-divine glaive-magic pot
-    itemthresholds = [[710, 1870, 2250, 2000, 2020, 3000, 1500], [710, 1870, 450, 2000, 2020, 3000, 1730, 1500]]
-    itemMP1 = [[0, 50, 75, 75, 70, 185, 30],[0, 50, 8, 75, 70, 185, 52, 30]]
+    itemthresholds = [[710, 1870, 2250, 2000, 2020, 3000, 1500], [710, 1870, 450, 2100, 2000, 2020, 2760, 1500]]
+    itemMP1 = [[0, 50, 75, 75, 70, 185, 30],[0, 50, 8, 90, 75, 70, 177, 30]]
     itemCD = [[0.10, 0.20, 0.10, 0, 0, 0, 0], [0.10, 0.20, 0.05, 0, 0, 0, -0.05, 0.10]]
-    itemnames = [["CDboots", "CDbook", "LT", "GW", "CE", "HC", "Mpot"],["CDboots", "CDbook", "BoS", "GW", "CE", "HC", "DG", "Mpot"]]
+    itemnames = [["CDboots", "CDbook", "LT", "GW", "CE", "HC", "Mpot"],["CDboots", "CDbook", "BoS", "BW", "GW", "CE", "HC", "Mpot"]]
 
     items_toggled = []
 
@@ -45,20 +45,19 @@ for build_id in build_ids:
     update_flag = True
 
 
-    for milli in range(1200000):
+    for milli in range(1500000):
         #check for purchases
         LTcooldown -= 1
         if milli%100==0:
             gold += 1
-        for itemid, threshold in enumerate(itemthresholds[build_id]):
-            goldcost = 0
-            goldcost += itemthresholds[build_id][itemid]
-            if gold > goldcost:
-                magicpower += itemMP1[build_id][itemid]
-                cdr += itemCD[build_id][itemid]
-                if itemnames[build_id][itemid] not in items_toggled:
-                    items_toggled.append(itemnames[build_id][itemid])
-                    print(itemnames[build_id][itemid])
+        if itemthresholds[build_id] != []:
+            if gold > itemthresholds[build_id][0]:
+                gold -= itemthresholds[build_id].pop(0)
+                magicpower += itemMP1[build_id].pop(0)
+                cdr += itemCD[build_id].pop(0)
+                if itemnames[build_id][0] not in items_toggled:
+                    print(itemnames[build_id][0])
+                    items_toggled.append(itemnames[build_id].pop(0))
                     update_flag = True
         #check for levels
         if (milli+1)%6000 == 0 and level < 15:
@@ -85,9 +84,11 @@ for build_id in build_ids:
             if skilllevel > 0 and skillcount[j] > skillcds[j][skilllevel-1]*1000*(1-cdr)+skilllag*1000:
                 castcount[j] += 1
                 if "LT" in items_toggled and LTcooldown <= 0:
-                    damage_total += 120*magicpower*120/(120+enemy_mr-mpen)*2
-                else:
+                    damage_total += 1.2*magicpower*120/(120+enemy_mr-mpen)*2
+                    LTdamage = 1.2*magicpower*120/(120+enemy_mr-mpen)*2
                     LTcooldown = 6000
+                    if milli > 930000 and milli < 960000:
+                        print(f"LT used for {LTdamage}")
                 if six_hit_count_conc_energy > 5:
                     magicdamagemult = 1.12
                     six_hit_count_conc_energy = 0
@@ -105,6 +106,9 @@ for build_id in build_ids:
 
                 skillcount[j] = 0
                 damage_total += (skilldmg[j][skilllevel-1]+skillscaling[j]*magicpower)*120/(120+enemy_mr-mpen)*AoE[j]
+                skill_dmg_check = (skilldmg[j][skilllevel-1]+skillscaling[j]*magicpower)*120/(120+enemy_mr-mpen)*AoE[j]
+                if milli > 930000 and milli < 960000:
+                    print(f"skill cast for {skill_dmg_check}")
                 for skilli in range(3):
                     for hit in range(multihit[j]):
                         skillcount[skilli] += 500
@@ -114,12 +118,12 @@ for build_id in build_ids:
             damage_ys[build_id].append(damage_total)
 
 
-x = [i*100 for i in range(12000)]
+x = [i*100 for i in range(15000)]
 
 # plot lines
 plt.plot(x, damage_ys[0], label = "Lightning Truncheon Build")
 plt.plot(x, damage_ys[1], label = "Rush CD Build")
-plt.xlabel("seconds")
+plt.xlabel("milliseconds")
 plt.ylabel("damage")
 plt.legend()
 plt.show()
